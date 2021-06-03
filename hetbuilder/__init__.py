0a3e454744c560cf8466e91ea390c202f4393413
Roman Kempt <roman.kempt@gmail.com>
2021-06-03 10:56:33 +0200

    recursive-include in MANIFEST.in

--- hetbuilder/__init__.py
+++ hetbuilder/__init__.py
@@ -7,7 +7,7 @@ from pathlib import Path
 if sys.version_info[0] == 2:
     raise ImportError("Requires Python3. This is Python2.")
 
-__version__ = "0.6.72"
+__version__ = "0.6.73"
 
 PROJECT_ROOT_DIR = Path(__file__).absolute().parent
 
